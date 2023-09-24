e0aba768a77459c2cdb5adbdd1152012abc810ad
Pfaffenrodt <dimitri@pfaffenrodt.de>
2023-09-24 23:07:34 +0200

    👷 build exe via github actions

--- main.c
+++ main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <Windows.h>
+#include <windows.h>
 
 
 typedef char(__cdecl* StartServer_S)(char*, int, int, int);
@@ -8,15 +8,15 @@ int main(int argc, char* argv[])
 {
     HINSTANCE server = LoadLibraryA("server.dll");
     if (!server) {
-        printf("Failed to load dll\n");
+        printf("Failed to load dll\n error: %s", GetLastError());
         return EXIT_FAILURE;
     }
 
     StartServer_S startServer = (StartServer_S)GetProcAddress(server, "StartServer");
-    if (startServer("SERV", 0, NULL, NULL) == 0) {
+    if (startServer("NLPT", 0, NULL, NULL) == 0) {
         printf("Failed to start server!\n");
     }
-    
+
     while (1) { Sleep(2000); }
 
     return 0;
